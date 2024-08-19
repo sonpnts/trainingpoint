5b3a87175ced1cc13a44e3bbada4bca8e2c55cb0
Nguyen Phong <105875447+Jen-ncode@users.noreply.github.com>
2024-08-19 17:42:11 +0700

    Tạo trang quản trị (trang admin)

--- trainingpointAPI/trainingpoint/admin.py
+++ trainingpointAPI/trainingpoint/admin.py
@@ -6,12 +6,24 @@ from ckeditor_uploader.widgets import CKEditorUploadingWidget
 class TaiKhoanAdmin(admin.ModelAdmin):
     readonly_fields = ['my_avatar']
 
-
-
     def my_avatar(self, taikhoan):
         if taikhoan.avatar:
             return mark_safe(f"<img width='200' src='{taikhoan.avatar.url}' />")
-
+            
+admin.site.register(TaiKhoan)
+admin.site.register(Khoa)
+admin.site.register(Lop)
+admin.site.register(TroLySinhVien_Khoa)
+admin.site.register(SinhVien)
+admin.site.register(Dieu)
+admin.site.register(BaiViet)
+admin.site.register(Comment)
+admin.site.register(Like)
+admin.site.register(HocKy_NamHoc)
+admin.site.register(ThamGia)
+admin.site.register(MinhChung)
+admin.site.register(HoatDongNgoaiKhoa)
+admin.site.register(DiemRenLuyen)
 
 
 
