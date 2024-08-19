bc1d6dcfc9111fcc7fa18cac1f7eacf2f671033d
Nguyen Phong <105875447+Jen-ncode@users.noreply.github.com>
2024-08-19 17:38:32 +0700

    Update admin.py

--- trainingpointAPI/trainingpoint/admin.py
+++ trainingpointAPI/trainingpoint/admin.py
@@ -13,18 +13,5 @@ class TaiKhoanAdmin(admin.ModelAdmin):
             return mark_safe(f"<img width='200' src='{taikhoan.avatar.url}' />")
 
 
-admin.site.register(TaiKhoan)
-admin.site.register(Khoa)
-admin.site.register(Lop)
-admin.site.register(TroLySinhVien_Khoa)
-admin.site.register(SinhVien)
-admin.site.register(Dieu)
-admin.site.register(BaiViet)
-admin.site.register(Comment)
-admin.site.register(Like)
-admin.site.register(HocKy_NamHoc)
-admin.site.register(ThamGia)
-admin.site.register(MinhChung)
-admin.site.register(HoatDongNgoaiKhoa)
-admin.site.register(DiemRenLuyen)
+
 
